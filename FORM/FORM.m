1fa2907c111560b53e73a9a8bcc868e22f5e08d2
siwalan <59278902+siwalan@users.noreply.github.com>
2022-07-15 17:40:45 +0800

    updates on step size

--- FORM/FORM.m
+++ FORM/FORM.m
@@ -5,8 +5,8 @@
 clc; clear;
 convergence = 0;
 
-marg = [1;1;1;1;1];
-parameter = [0.07433 0.005 0 0; 0.1 0.01 0 0; 13 60 0 0; 4751 48 0 0; -648 11 0 0];
+marg = [1;1;1;];
+parameter = [50 6.25 0 0; 60 3 0 0; 1000 200 0 0;];
 R = eye(size(parameter,1));
 
 Ro = mod_corr(R,marg,parameter);
@@ -15,8 +15,9 @@ iLo = inv(Lo);
 X = [parameter(:,1)]';
 U = x_to_u(X,marg,parameter,iLo)
 i = 1
-e1 = 10^-8;
-e2 = 10^-7;
+e1 = 10^-5;
+e2 = 10^-5;
+maxIter = 1000;
 while convergence == 0
     X = u_to_x(U,marg,parameter,Lo);
     J_u_x = jacobian(X,U,marg,parameter,Lo,iLo);
@@ -31,14 +32,15 @@ while convergence == 0
     normCurGrad = norm(curGrad);
     alpha = -curGrad/normCurGrad;
 
-    if ( (abs(Y/Y0)<e1) &&  (norm(U-alpha'*U*alpha)<e2))
+    if ( (abs(Y/Y0)<e1) &&  (norm(U-alpha'*U*alpha)<e2)) || i > maxIter
         convergence = 1
     end
 
     beta =  1 - normcdf(alpha' * U);
 
     d = (Y/normCurGrad + alpha' * U) * alpha - U;
-    U = U + d*0.001;
-    i = i + 1
+    U = U + d*1;
+    i = i + 1;
 end
 beta =  1 - normcdf(alpha' * U)
+u_to_x(U,marg,parameter,Lo)
