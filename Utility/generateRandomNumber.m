629113ec8f5d5cf16db872b58c1a8c73317f3d15
siwalan <59278902+siwalan@users.noreply.github.com>
2022-08-04 19:23:56 +0800

    Update generateRandomNumber.m

--- Utility/generateRandomNumber.m
+++ Utility/generateRandomNumber.m
@@ -1,11 +1,17 @@
-function X = genRV(marg, parameter)
-
+function X = genRV(marg, parameter, truncPositive)
+if ~exist('truncPositive','var')
+  truncPositive = 0;
+end
 RV_Numbers = size(marg,1);
 X = zeros(RV_Numbers,1);
 for RV_Distribution=1:RV_Numbers
     if (marg(RV_Distribution) == 1)
-        %% Normal Distribution
         X(RV_Distribution) = normrnd(parameter(RV_Distribution,1),parameter(RV_Distribution,2));
+        if truncPositive == 1
+            while(X(RV_Distribution) < 0)
+               X(RV_Distribution) = normrnd(parameter(RV_Distribution,1),parameter(RV_Distribution,2));
+            end
+        end
     else
         m = parameter(RV_Distribution,1); % mean
         v = parameter(RV_Distribution,2); % variance
